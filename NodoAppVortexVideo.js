14240f90b8355263929f91e5d3b53b5fe6a968e1
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2013-08-08 16:51:18 -0300

    sin login

--- NodoAppVortexVideo.js
+++ NodoAppVortexVideo.js
@@ -7,24 +7,9 @@ NodoAppVortexVideo.prototype.start = function(){
     this.ui = $('#plantilla_app_vortex_video').clone();
     this.router = new NodoRouter("app vxv");
     
-    var _this = this;
-    var vista_login = new VistaLogin({
-        callback_usuario: function(un_usuario){
-            _this.setUsuarioLogueado(un_usuario);
-        }
-    });
-    
     this.panel_amigos = this.ui.find('#panel_amigos');
     this.txt_nombre_amigo = this.ui.find('#txt_nombre_amigo');
     this.btn_crear_receptor = this.ui.find('#btn_crear_receptor');
-};
-
-NodoAppVortexVideo.prototype.setUsuarioLogueado = function(un_usuario){
-    this.usuarioLogueado = un_usuario;
-    
-    this.transmisor = new NodoTransmisorVideo({nombreUsuario: this.usuarioLogueado.nombre});
-    this.router.conectarBidireccionalmenteCon(this.transmisor);
-    this.transmisor.dibujarEn(this.panel_amigos);
     
     var _this = this;
     
@@ -33,6 +18,16 @@ NodoAppVortexVideo.prototype.setUsuarioLogueado = function(un_usuario){
         _this.router.conectarBidireccionalmenteCon(un_receptor);
         un_receptor.dibujarEn(_this.panel_amigos);
     });
+};
+
+NodoAppVortexVideo.prototype.setUsuarioLogueado = function(un_usuario){
+    //this.usuarioLogueado = un_usuario;
+    
+//    this.transmisor = new NodoTransmisorVideo({nombreUsuario: this.usuarioLogueado.nombre});
+//    this.router.conectarBidireccionalmenteCon(this.transmisor);
+//    this.transmisor.dibujarEn(this.panel_amigos);
+    
+
 };
 
 NodoAppVortexVideo.prototype.conectarCon = function(nodo){
