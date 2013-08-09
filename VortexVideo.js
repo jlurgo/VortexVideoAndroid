72aa6d0f813535ce71ca7afe824cabdc56ff98b0
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2013-08-09 05:05:43 -0300

    con login

--- VortexVideo.js
+++ VortexVideo.js
@@ -2,13 +2,13 @@ var onDeviceReady = function() {
     var router =  new NodoRouter("principal"); 
             
     //var clienteHTTP = new NodoClienteHTTP('http://localhost:3000', 100);             
-    var clienteHTTP = new NodoClienteHTTP('http://router-vortex.herokuapp.com', 100);             
-    router.conectarBidireccionalmenteCon(clienteHTTP);
+    //var clienteHTTP = new NodoClienteHTTP('http://router-vortex.herokuapp.com', 100);             
+    //router.conectarBidireccionalmenteCon(clienteHTTP);
     
     //var socket = io.connect('http://localhost:3000');
-    //var socket = io.connect('http://router-vortex.herokuapp.com');
-    //var conector_socket = new NodoConectorSocket(socket);
-    //router.conectarBidireccionalmenteCon(conector_socket);
+    var socket = io.connect('http://router-vortex.herokuapp.com');
+    var conector_socket = new NodoConectorSocket(socket);
+    router.conectarBidireccionalmenteCon(conector_socket);
     
     var nodo_app_vxv = new NodoAppVortexVideo();        
     router.conectarBidireccionalmenteCon(nodo_app_vxv);   
